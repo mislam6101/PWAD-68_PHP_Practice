86dfd5a1cf1049267e8eb46afa2ccd8bc5f40ce1
mislam6101 <m.islam6101@gmail.com>
2025-10-22 09:22:39 +0600

    Test File

---
+++ Test/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Welcome to PHP</h3>
+
+    <?php 
+    
+    print "Hello World!";
+    
+
+    ?>
+    
+
+</body>
+</html>
