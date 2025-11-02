95ae83364f3f81559bcf9beff23304b52f9a4fb7
mislam6101 <m.islam6101@gmail.com>
2025-11-02 13:02:03 +0600

    02.11.2025

---
+++ Class_Practice/heredoc.php
@@ -0,0 +1,9 @@
+<?php
+$website = "https://www.prothomalo.com/";
+echo <<<neer
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, repudiandae! <a href="$website" target="_blank">Come</a></p>
+<h2>This is a ....</h1>
+
+neer;
+
+?>
---
+++ Class_Practice/include_&_require.php
@@ -0,0 +1,16 @@
+<?php 
+
+include "form.php"; // if file is missing we got a simple warning error.
+include_once "form.php"; // if file is missing we got a simple warning error.
+include "form.php";
+
+
+?>
+<hr>
+
+<?php 
+
+require_once "form.php"; //if file is missing we got a fetal error.
+require "form.php"; //if file is missing we got a fetal error.
+
+?>
