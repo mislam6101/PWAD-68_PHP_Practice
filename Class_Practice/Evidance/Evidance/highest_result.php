e42ffbe391445e81a8deccf1efe1ff4e33d721fd
mislam6101 <m.islam6101@gmail.com>
2025-11-16 13:02:49 +0600

    16.11.2025

---
+++ Class_Practice/Evidance/Evidance/highest_result.php
@@ -0,0 +1,22 @@
+<?php 
+
+$result = [
+    "Aarif" => 79,
+    "Jarin" => 78,
+    "Rafi" => 79,
+    "Sakib" => 81
+];
+
+$max_result = 0;
+foreach($result as $value){
+    if($value > $max_result){
+        $max_result = $value;
+    }
+}
+foreach($result as $key => $value){
+    if($value == $max_result){
+        echo "Name : " . $key . " Result : " . $value . "<br>";
+    }
+}
+
+?>
